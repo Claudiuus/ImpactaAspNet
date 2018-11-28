6a0a696d1d7acd5beb4396c5ef2064840ccddaf6
Claudio.Sousa <claudiuus.a90@gmail.com>
2018-11-27 22:24:55 -0200

    27/11/2018

--- Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
+++ Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -8,13 +8,16 @@ using System.Threading.Tasks;
 
 namespace Oficina.Repositorios.SistemaArquivos
 {
+    
     public class CorRepositorio
     {
+        const string caminhoArquivo = "Dados\\Cor.txt";
+
         public List<Cor>Selecionar()
         {
             var cores = new List<Cor>();
 
-            foreach (var item in File.ReadAllLines("Dados\\Cor.txt"))
+            foreach (var item in File.ReadAllLines(caminhoArquivo))
             {
                 var cor = new Cor();
                 cor.Id = Convert.ToInt32(item.Substring(0,5));//Onde Finaliza
@@ -24,6 +27,28 @@ namespace Oficina.Repositorios.SistemaArquivos
             }
             return cores;
         }
-       
+
+
+
+        public Cor Selecionar(int id)
+        {
+            Cor cor = null;
+
+            foreach (var item in File.ReadAllLines(caminhoArquivo))
+            {
+                var linhaId = item.Substring(0, 5);
+                if (Convert.ToUInt32(linhaId) == id)
+                {
+                    cor = new Cor();
+
+                    cor.Id = Convert.ToInt32(item.Substring(0, 5));//Onde Finaliza
+                    cor.Nome = item.Substring(5);//Onde inicia
+                    break;
+                }
+            }
+
+            return cor;
+        }
+
     }
 }
---
+++ Oficina.Repositorios.SistemaArquivos/MarcaRepositorio.cs
@@ -0,0 +1,55 @@
+﻿using Oficina.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+  public  class MarcaRepositorio
+    {
+        private string caminhoArquivo = ConfigurationManager.AppSettings["caminhoArquivoMarca"];
+
+        public List<Marca> Selecionar()
+        {
+            var marcas = new List<Marca>();
+
+            foreach (var item in File.ReadAllLines(caminhoArquivo))
+            {
+                var propriedades = item.Split('|');
+                var marca = new Marca();
+
+                marca.Id = Convert.ToInt32(propriedades[0]);//Onde Finaliza
+                marca.Nome = propriedades[1];//Onde inicia
+
+                marcas.Add(marca);
+            }
+            return marcas;
+        }
+
+        public Marca Selecionar(int id)
+        {
+            Marca marca = null;
+
+            foreach (var item in File.ReadAllLines(caminhoArquivo))
+            {
+                var propriedades = item.Split('|');
+                if (Convert.ToUInt32(propriedades[0]) == id)
+                {
+                    
+                    marca = new Marca();
+
+                    marca.Id = Convert.ToInt32(propriedades[0]);//Onde Finaliza
+                    marca.Nome = propriedades[1];//Onde inicia
+                    break;
+                }
+            }
+
+            return marca;
+        }
+    }
+}
